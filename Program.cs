b4775975734ea0c8d34bf4027bd0223e744a3ba0
Muhammad Abdullah Kiani <22MKiani@heckgrammar.co.uk>
2025-05-13 10:00:11 +0100

    njgfjf

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool valid = true;
+            do
+            {
+                Console.WriteLine("Input a hexcode: ");
+                string us_in = Console.ReadLine();
+
+                if (us_in.Length != 7)
+                {
+                    Console.WriteLine("Invalid input, try again - input length");
+                    valid = false;
+                }
+                if (us_in.Substring(0,1) != "£" && us_in.Substring(0,1)!= "#")
+                {
+
+                }
+            }
+            while (valid!=true);
+
+
         }
     }
 }
