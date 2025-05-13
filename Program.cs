ce875e46f8fa3e19cc9729d20e72abf38943d852
Muhammad Abdullah Kiani <22MKiani@heckgrammar.co.uk>
2025-05-13 09:51:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hex_challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
