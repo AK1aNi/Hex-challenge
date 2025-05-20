aed6ea26d075e5e3acd0ded75344d2bd7416632b
Muhammad Abdullah Kiani <22MKiani@heckgrammar.co.uk>
2025-05-20 09:59:42 +0100

    gsdgsg

--- Program.cs
+++ Program.cs
@@ -5,16 +5,33 @@
         static void Main(string[] args)
         {
             bool valid = true;
+            int char_inv = 0;
             do
             {
                 Console.WriteLine("Input a hexcode: ");
                 string us_in = Console.ReadLine();
-
+                char[] checker = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'];
+                
                 if (us_in.Length == 7)
                 {
-                    if (us_in.Substring(0,1) == "£" && us_in.Substring(0,1) == "#")
+                    if (us_in[0] == '£' || us_in[0] == '#')
                     {
-                        Console.WriteLine("gid");
+                        for (int i = 1; i < 7; i++)
+                        {
+                            if (checker.Contains(us_in[i]))
+                            {
+                                char_inv = char_inv + 1;
+                            }
+
+                        }
+                        if (char_inv == 6)
+                        {
+                            // £123456→
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input, not a hexcode");
+                        }
                     }
                     else
                     {
