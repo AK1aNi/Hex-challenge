a3c880751324db344e4a06eff96829049facce45
Muhammad Abdullah Kiani <22MKiani@heckgrammar.co.uk>
2025-05-20 09:17:35 +0100

    fsfs

--- Program.cs
+++ Program.cs
@@ -10,14 +10,22 @@
                 Console.WriteLine("Input a hexcode: ");
                 string us_in = Console.ReadLine();
 
-                if (us_in.Length != 7)
+                if (us_in.Length == 7)
+                {
+                    if (us_in.Substring(0,1) == "£" && us_in.Substring(0,1) == "#")
+                    {
+                        Console.WriteLine("gid");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input, try again - first char");
+                        valid = false;
+                    }
+                }
+                else
                 {
                     Console.WriteLine("Invalid input, try again - input length");
                     valid = false;
-                }
-                if (us_in.Substring(0,1) != "£" && us_in.Substring(0,1)!= "#")
-                {
-
                 }
             }
             while (valid!=true);
